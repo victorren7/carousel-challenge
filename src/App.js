50d3fb82e9d478683cf821df4b7694f69863b345
victor <31532226+victorren7@users.noreply.github.com>
2020-07-20 17:31:33 -0700

    trying to fix map

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
 import Rotator from './Rotator';
 import data from './data.json'
+import Vector from './assets/Vector.png';
+import Vector2 from './assets/Vector2.png'
+import {map} from 'lodash';
 
 const firstGroup = data[0].quotes
-console.log('firstGroup', firstGroup[0])
+const secondGroup = data[1].quotes
+console.log(data[0].quotes)
+console.log(map(firstGroup, 'message', 'attribution'))
 
 function App() {
   return (
-    <div className="App">
-      <Rotator group={firstGroup[0]} />
+    <div>
+      <Card className="App">
+        <Image 
+          src={Vector}
+          left={67.88} 
+          top={1.69}
+          bottom={58.67}
+        />
+          {/* <Rotator group={map(firstGroup, 'message')} /> */}
+        <Rotator group={firstGroup[0]} />
+      </Card>
+      <Card>
+        <Image 
+          src={Vector2} 
+          right={67} 
+          top={49.2}
+          bottom={11.62}
+        />
+        <Rotator group={secondGroup[0]}/> 
+      </Card>
     </div>
   );
 }
+
+const Card = styled.div`
+  position: relative;
+   height: 100vh;
+`;
+
+const Image = styled.img`
+  position: absolute;
+  left: ${prop => prop.left ? prop.left : 0}%;
+  right: ${prop => prop.right ? prop.right : 0}%;
+  top: ${prop => prop.top ? prop.top : 0}%;
+  bottom: ${prop => prop.bottom ? prop.bottom : 0}%;
+  height: 860px;
+`;
 
 export default App;
--- src/Rotator.js
+++ src/Rotator.js
@@ -1,34 +1,74 @@
-import React, {Fragment} from 'react';
-import Carousel from "@brainhubeu/react-carousel";
-import {map, filter, find} from 'lodash';
+import React from 'react';
+import {map} from 'lodash';
+import styled from 'styled-components';
+import Carousel, { arrowsPlugin } from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
+import next from './assets/next.svg';
+import back from './assets/back.svg';
 
 const Rotator = ({group}) => {
   
   return(
     <div>
-      <Carousel arrows infinite>
+      <CarouselStyle 
+        arrows 
+        infinite
+        // plugins={[
+        //   {
+        //     resolve: arrowsPlugin,
+        //     options: {
+        //       arrowLeftDisabled:<button><img src={back} /></button>,
+        //       arrowLeft: <button><img src={back} /></button>,
+        //       arrowRight: <button><img src={next}/></button>,
+        //       arrowRightDisabled: <button><img src={back} /></button>,
+
+        //       addArrowClickHandler: true,
+        //     }
+        //   }
+        // ]}
+      >
         {map({group}, (quote, index) => {
           return (
-            <Fragment>
-              <div className="quote">
-                <p>
+            <Container key={index}>
+              <Wrapper>
+                <Text fontSize={72}>
                   {quote.message}
-                </p>
-              </div>
-              <div className="quote">
-                <p>
+                </Text>
+                <HR />
+                <Text fontSize={56}>
                   {quote.attribution}
-                </p>    
-              </div>
-            </Fragment>
+                </Text>  
+              </Wrapper> 
+            </Container>
           )
         })}
-      </Carousel>
+      </CarouselStyle>
 
     </div>
   )
 };
+
+const CarouselStyle = styled(Carousel)`
+  width: 90%;
+  margin: auto;
+`;
+
+const Container = styled.div`
+display: grid;
+width: 1130px;
+text-align: center;
+`;
+const Wrapper = styled.div`
+  position: relative
+`;
+
+const Text = styled.p`
+  font-size: ${props => props.fontSize ? props.fontSize : 16}px;
+`; 
+
+const HR = styled.hr`
+  width: 20px;
+`;
 
 
 
